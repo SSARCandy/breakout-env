716252e9cb83a42950b022b86e6d13db2732eaad
ssarcandy <ssarcandy@gmail.com>
2018-02-08 23:37:04 +0800

    publish v1.0.3

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 def read_readme():
-    with open('README.md') as f:
-        return f.read()
+  with open('README.md') as f:
+    return f.read()
 
 setup(
   name = 'breakout_env',
-  packages = ['breakout_env'], # this must be the same as the name above
-  version = '1.0.0',
+  packages=find_packages(),
+  version='1.0.3',
   description = 'A configurable Breakout environment for reinforcement learning',
   long_description = read_readme(),
   author = 'SSARCandy',
@@ -17,6 +17,6 @@ setup(
   # download_url = 'https://github.com/SSARCandy/breakout-env/archive/1.0.0.tar.gz', # I'll explain this in a second
   keywords = ['game', 'learning', 'evironment'], # arbitrary keywords
   classifiers = [],
-  install_requires=['numpy>=1.1'],
+  install_requires=['numpy>=1.1', 'distribute'],
   include_package_data=True
 )
